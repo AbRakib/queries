89ac69b30a9dce979d7e3da119df8e496a7b1b63
AbRakib <abrakib.cse@gmail.com>
2023-08-30 12:24:20 +0600

    whereRaw

--- routes/web.php
+++ routes/web.php
@@ -65,12 +65,36 @@ Route::get( '/', function () {
     // $result = DB::table( 'rooms' )
     //     ->whereBetween( 'room_size', [1, 3] )
     //     ->get();
-    
+
     // $result = DB::table('rooms')
     //     ->whereNotIn('id', [1,2,3])  // without this 1,2,3 id get data
     //     ->get();
 
     // dump( $result );
 
+    // WhereNull('column')  whereNotNull()
+    // WhereData('created_at', '2020-05-13')
+    // whereMonth('created-at', '5')
+    // whereDay('created_at', '24');
+    // whereYear('created_at', '2020');
+    // whereTime('created_at', '=', '12:25:10');
+    // whereColumn('column1', '>', 'column2');
+
+    // whereColumn([
+    //     ['first_name', '=', 'last_name'],
+    //     ['updated_at', '=', 'created_at'],
+    // ]);
+
+    $result = DB::table( 'users' )
+        ->whereExists( function ( $query ) {
+            $query->select( 'id' )
+                ->from( 'reservations' )
+                ->whereRaw( 'reservations.user_id = users.id' )
+                ->whereDate( 'created_at', '=', '2023-08-30' )
+                ->limit( 1 );
+        } )
+        ->get();
+
+    dump( $result );
     return view( 'welcome' );
 } );
